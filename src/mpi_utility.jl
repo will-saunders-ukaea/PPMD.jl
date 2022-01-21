e217a96d8200849cb4450795753f0df2d9e88ff3
Will Saunders <wrs@wrsaunders.co.uk>
2022-01-21 11:06:56 +0000

    MPI Win memory allocation is now allocated with MPI_Alloc_mem to be portable

--- src/mpi_utility.jl
+++ src/mpi_utility.jl
@@ -1,6 +1,76 @@
-
+export alloc_mem, free_mem, MPIArray, free
 
 using MPI
+
+
+"""
+Allocate memory using MPI_Alloc_mem which is required for portable MPI.
+"""
+function alloc_mem(size, dtype)
+
+    ptr_ref = Ref{Ptr{Cchar}}()
+    size_bytes = size * sizeof(dtype)
+
+    # int MPI_Alloc_mem(MPI_Aint size, MPI_Info info, void *baseptr)
+    MPI.@mpichk ccall(
+        (:MPI_Alloc_mem, libmpi), 
+        Cint,
+        (MPI.MPI_Aint, MPI.MPI_Info, Ref{Ptr{Cchar}}),
+        MPI.MPI_Aint(size_bytes), 
+        MPI.Info(), 
+        ptr_ref
+    )
+    
+    ptr = Ptr{Cchar}(ptr_ref[])
+    ptr = convert(Ptr{dtype}, ptr)
+
+    return ptr
+end
+
+
+"""
+Free memory allocated with MPI_Alloc_mem
+"""
+function free_mem(ptr)
+
+    ptr = convert(Ptr{Cchar}, ptr)
+
+    # int MPI_Free_mem(void *base)
+    MPI.@mpichk ccall(
+        (:MPI_Free_mem, libmpi),
+        Cint,
+        (Ptr{Cchar},),
+        ptr
+    )
+
+end
+
+
+"""
+Allocate an Array with MPI and wrap in an Array.
+"""
+mutable struct MPIArray
+    size
+    dtype
+    array
+    ptr
+    function MPIArray(size, dtype)
+        ptr = alloc_mem(size, dtype)
+        arr = unsafe_wrap(Array{dtype}, ptr, size)
+        return new(size, dtype, arr, ptr)
+    end
+end
+
+
+"""
+Free an MPIArray.
+"""
+function free(arr::MPIArray)
+    arr.size = 0
+    arr.array = zeros(arr.dtype, 0)
+    free_mem(arr.ptr)
+    arr.ptr = Ptr{arr.dtype}(0)
+end
 
 
 """
@@ -8,14 +78,16 @@ Wrapper around an MPI Win. Must be created collectively on the MPI
 Communicator.
 """
 mutable struct Win
+    mpiarray::MPIArray
     buffer::Array
     comm::MPI.Comm
     win::MPI.Win
     function Win(comm, size, dtype)
-        # TODO potentially not portable as memory not allocated with MPI_Alloc_mem
-        buffer = zeros(dtype, size)
+        mpiarray = MPIArray(size, dtype)
+        buffer = mpiarray.array
+        buffer[:] .= 0
         win = MPI.Win_create(buffer, comm)
-        return new(buffer, comm, win)
+        return new(mpiarray, buffer, comm, win)
     end
 end
 
@@ -25,4 +97,24 @@ Free a Win object. Must be called collectively on the MPI communicator.
 """
 function free(win::Win)
     MPI.free(win.win)
+    free(win.mpiarray)
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/neighbour_transfer.jl
+++ src/neighbour_transfer.jl
@@ -36,48 +36,49 @@ function exchange_neighbour_ranks(particle_group, neighbour_ranks::Array{Cint})
 
     # for each remote rank increment the remote counter to find the remote offset
     # for this rank to place its rank in the remote buffer
-    recv_counts = zeros(Cint, 1)
-    # TODO potentially not portable as memory not allocated with MPI_Alloc_mem
-    recv_win = MPI.Win_create(recv_counts, comm)
+    recv_win = Win(comm, 1, Cint)
     remote_offsets = Array{Cint}(undef, N_remote_ranks)
     Cint_one = ones(Cint, 1)
     for rankx in 1:N_remote_ranks
         ranki = neighbour_ranks[rankx]
-        MPI.Win_lock(MPI.LOCK_SHARED, ranki, 0, recv_win)
+        MPI.Win_lock(MPI.LOCK_SHARED, ranki, 0, recv_win.win)
         MPI.Get_accumulate(
             view(Cint_one, 1:1),
             view(remote_offsets, rankx:rankx),
             ranki, 
             0, 
             MPI.SUM,
-            recv_win
+            recv_win.win
         )
-        MPI.Win_unlock(ranki, recv_win)
+        MPI.Win_unlock(ranki, recv_win.win)
     end
     MPI.Barrier(comm)
-    MPI.free(recv_win)
+    recv_count = recv_win.buffer[1]
+    free(recv_win)
     
     # allocate space and an MPI Window for the remote ranks
-    sending_ranks = Array{Cint}(undef, recv_counts[1])
-    # TODO potentially not portable as memory not allocated with MPI_Alloc_mem
-    sending_ranks_win = MPI.Win_create(sending_ranks, comm)
+    sending_ranks_win = Win(comm, recv_count, Cint)
 
     # reuse this temp array to send the rank
     Cint_one[1] = rank
     # place this rank integer in the remote window
     for rankx in 1:N_remote_ranks
         ranki = neighbour_ranks[rankx]
-        MPI.Win_lock(MPI.LOCK_SHARED, ranki, 0, sending_ranks_win)
+        MPI.Win_lock(MPI.LOCK_SHARED, ranki, 0, sending_ranks_win.win)
         MPI.Put(
             view(Cint_one, 1:1),
             ranki, 
             remote_offsets[rankx], 
-            sending_ranks_win
+            sending_ranks_win.win
         )
-        MPI.Win_unlock(ranki, sending_ranks_win)
+        MPI.Win_unlock(ranki, sending_ranks_win.win)
     end
     MPI.Barrier(comm)
-    MPI.free(sending_ranks_win)
+
+    sending_ranks = Array{Cint}(undef, recv_count)
+    sending_ranks[:] .= sending_ranks_win.buffer[:]
+
+    free(sending_ranks_win)
     
     return sending_ranks
 end
